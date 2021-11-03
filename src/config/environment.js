b17efa7c726bc13ae41d4f5fbff006ade9ee61fc
mauricio martinez <maumarcastil@gmail.com>
2021-11-03 17:38:13 -0500

    update url back

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,4 @@
 export const env = {
   apiAuth: "http://dev.nexttruck.draketechdev.ca:3600/api/login ",
-  back: "https://crudcrud.com/api/5345fd03926c4bb2a97e7bbe0027b6f7",
+  back: "https://crudcrud.com/api/ce2c54f9648644f3b5acb2a608d67674",
 };
